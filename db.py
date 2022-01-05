65c79afcbd6c5b57f842b40f94e5493a8420daee
varvara-ali <varvara.alimova@gmail.com>
2022-01-05 13:19:47 +0300

    add 3 tables

--- db.py
+++ db.py
@@ -3,16 +3,31 @@ import sqlite3
 
 class Statistic:
     def __init__(self):
-        self.con = sqlite3.connect('leaders')
         self.conn = None
         try:
-            self.conn = sqlite3.connect('leaders')
+            self.conn = sqlite3.connect('stat')
             print(sqlite3.version)
         except Exception as e:
             print(e)
 
     def create_db(self):
-        pass
+        leaders_creation = """
+        CREATE TABLE IF NOT EXISTS "leaders" (
+        "name"	TEXT, "record" INTEGER, "record_time" DATETIME)
+        """
+        session_table = """
+                CREATE TABLE IF NOT EXISTS "table" (
+                "cell" INTEGER, "value" INTEGER)
+                """
+        session_account = """
+                        CREATE TABLE IF NOT EXISTS "account" (
+                        "name" TEXT, "record" INTEGER)
+                        """
+        cur = self.con.cursor()
+        result = cur.execute(leaders_creation)
+        result = cur.execute(session_table)
+        result = cur.execute(session_account)
+        self.con.commit()
 
     def get_leaders(self, count=10):
         pass
--- main.py
+++ main.py
@@ -4,86 +4,7 @@ from start_screen import (Start_screen,
                           WIDTH,
                           HEIGHT
                           )
-
-class Cell:
-    def __init__(self, size, index_x, index_y, start_x, start_y):
-        self.size = size
-        self.x = index_x
-        self.y = index_y
-        self.start_x = start_x
-        self.start_y = start_y
-        self.background_color = 'black'
-        self.border_color = 'white'
-        self.alive = False
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.background_color, (self.start_x, self.start_y, self.size, self.size), 0)
-        pygame.draw.rect(screen, self.border_color, (self.start_x, self.start_y, self.size, self.size), 2)
-
-    def on_click(self):
-        self.change_state()
-
-    def change_state(self):
-        if not self.alive:
-            self.background_color = 'green'
-            self.alive = True
-        elif self.alive:
-            self.background_color = 'black'
-            self.alive = False
-
-    def check_neighbours(self):
-        pass
-
-
-class Board:
-    def __init__(self, cells_x, cells_y):
-        self.cells_x = cells_x
-        self.cells_y = cells_y
-        self.cell_size = 20  # размер клетки
-        self.margin_x = 50
-        self.margin_y = 50
-        self.cells = [[Cell(self.cell_size, x, y, self.margin_x + x * self.cell_size, self.margin_y + y * self.cell_size)
-                       for x in range(self.cells_x)] for y in range(self.cells_y)]
-
-    def draw(self, screen):
-        for cells_row in self.cells:
-            for cell in cells_row:
-                cell.draw(screen)
-
-    def is_coords_within(self, x: int, y: int) -> bool:
-        if self.margin_x <= x <= self.margin_x + (self.cells_x * self.cell_size) and \
-                self.margin_y <= y <= self.margin_y + (self.cells_y * self.cell_size):
-            return True
-        else:
-            return False
-
-    def get_cell_index_by_coords(self, x, y) -> tuple:
-        cell_by_x = (x - self.margin_x) // self.cell_size
-        cell_by_y = (y - self.margin_y) // self.cell_size
-        return cell_by_x, cell_by_y
-
-    def on_click(self, cell_x, cell_y):
-        self.cells[cell_y][cell_x].on_click()
-
-    def evolve(self):
-        current_state = copy.deepcopy(self.cells)
-        new_state = copy.deepcopy(self.cells)
-        # сама эволюция
-        for y in range(self.cells_y):
-            for x in range(self.cells_x):
-                neighbours = 0
-                for diff_y in [-1, 0, 1]:
-                    for diff_x in [-1, 0, 1]:
-                        if not (diff_x == 0 and diff_y == 0):
-                            if x + diff_x in range(self.cells_x) and y + diff_y in range(self.cells_y) and current_state[y + diff_y][x + diff_x].alive:
-                                neighbours += 1
-                if not new_state[y][x].alive and neighbours == 3:
-                    new_state[y][x].change_state()
-                elif new_state[y][x].alive and neighbours not in (2, 3):
-                    new_state[y][x].change_state()
-        self.cells = copy.deepcopy(new_state)
-        if new_state == current_state:
-            pygame.quit()
+from db import Statistic
 
 
 if __name__ == '__main__':
@@ -96,6 +17,7 @@ if __name__ == '__main__':
     fps = 60
 
     start_screen = Start_screen(screen)
+    stat = Statistic()
     # start_screen.hello()
 
     running = True
